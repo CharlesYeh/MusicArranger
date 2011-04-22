397a8e5801d7ddeb380df62299ffd26a8d1a8565
Charles Yeh <charles_yeh@brown.edu>
2011-04-22 18:12:33 -0400

    Music classes and images added.

---
+++ music/MultiNote.java
@@ -0,0 +1,15 @@
+package music;
+
+import java.util.LinkedList;
+
+public class MultiNote {
+	int _durationNumer, _durationDenom;
+	
+	LinkedList<Pitch> _notes;
+	
+	public MultiNote() {
+		_durationNumer = _durationDenom = 1;
+		
+		_notes = new LinkedList<Pitch>();
+	}
+}
