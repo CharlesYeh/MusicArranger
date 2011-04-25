4c71dab27858707dd50469093954a1b910a9d531
Evan Li <evanyeejinli@gmail.com>
2011-04-24 20:21:37 -0400

    Fixed some constructors.

--- music/MultiNote.java
+++ music/MultiNote.java
@@ -8,9 +8,9 @@ import java.util.ArrayList;
 public class MultiNote extends Duration {
 	ArrayList<Pitch> _pitches;					// all pitches in the MultiNote, from highest to lowest; an empty list of Pitches represents a rest.
 	
-	public MultiNote(Rational duration, ArrayList<Pitch> pitches) {
+	public MultiNote(Rational duration) {
 		super(duration);
-		_pitches = pitches;
+		_pitches = new ArrayList<Pitch>();
 	}
 	
 	public ArrayList<Pitch> getPitches() {
